c200498301ed0d8b3249e1f269b0a13336e97666
chizidotdev <chizivwokoma@gmail.com>
2022-12-27 22:33:11 +0100

    chore: update package names to smhos

--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -1,4 +1,4 @@
-import { appRouter, createContext } from "@acme/api";
+import { appRouter, createContext } from "@smhos/api";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
 // export API handler
